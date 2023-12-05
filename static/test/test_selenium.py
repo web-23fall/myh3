e01ac12d33a94fc71335be9af746b9c4c65fe01f
Dusker233 <1187305740@qq.com>
2023-12-05 13:21:33 +0800

    20231205-6

--- static/test/test_selenium.py
+++ static/test/test_selenium.py
@@ -3,10 +3,6 @@ from selenium.webdriver.common.by import By
 import time
 import chromedriver_autoinstaller
 
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
-
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
 # and if it doesn't exist, download it automatically,
 # then add chromedriver to path
